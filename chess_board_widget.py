6bf4f01ca3e3ab494f3cba259320bb827ed0406a
Andrei Oshukov <andrei.oshukov@jetbrains.com>
2023-10-25 01:53:11 +0600

    minor refactor, removed comments

--- chess_board_widget.py
+++ chess_board_widget.py
@@ -11,23 +11,19 @@ from uuid import uuid4
 from Ilmarinen.widgethub import Event, WidgetHub
 from Ilmarinen.database_widget import DatabaseWidget
 
+
 class GameState:
     def __init__(self, parent):
         self.board = chess.Board()
         self.game = chess.pgn.Game()
         self.parent = parent
-        print('I am gamestate in __init__')
 
     def move_piece(self, start_square, end_square):
-        # print(f'Attempting move {start_square+end_square}')
         try:
             move = chess.Move.from_uci(start_square + end_square)
         except chess.InvalidMoveError:
             return False
         if move in self.board.legal_moves:
-            # print(f"Making move {move}")
-            # self.board.push(move)
-            # print(f"Currently child notation is {self.parent.child_notation}")
             try:
                 self.parent.hub.produce_event(Event.GameMove, move=move)
             except Exception as e:
@@ -42,7 +38,6 @@ class ChessSquare(QGraphicsRectItem):
         # 0, 0 by default is a8 1,0 is a7
         self.square_name = chr(ord('a')+x) + str(8-y)
         self.coordinates = (x, y)
-        # self.setFlags(self.flags() | QGraphicsItem.GraphicsItemFlag.ItemIsSelectable)
 
 
 class Chessboard(QGraphicsView):
@@ -79,7 +74,6 @@ class Chessboard(QGraphicsView):
         self.draw_board()
         self.child_notation = None
         self.hub.produce_event(Event.BoardCreated, board=self)
-        # self.flip_board()
 
     def board_move_event(self, move: chess.Move):
         self.game_state.board.push(move)
@@ -87,6 +81,7 @@ class Chessboard(QGraphicsView):
     def set_child_notation(self, notation):
         print(f"{self} had its notation child set to {notation}")
         self.child_notation = notation
+
     def resizeEvent(self, event):
         self.setSceneRect(QRectF(self.viewport().rect()))
         self.redraw_board()
@@ -95,7 +90,6 @@ class Chessboard(QGraphicsView):
     def reset_board(self):
         self.game_state = GameState(self)
         self.refresh_board()
-
 
     def flip_board(self):
         self.flipped = not self.flipped
@@ -135,10 +129,8 @@ class Chessboard(QGraphicsView):
         super().mousePressEvent(event)
 
     def refresh_board(self, **kwargs):
-        # print('-----------'*3)
         for i in range(8):
             for j in range(8):
-                # remove the previous piece on this square if it exists
                 if self.pieces[i][j] is not None:
                     self.scene.removeItem(self.pieces[i][j])
                     self.pieces[i][j] = None
@@ -146,10 +138,7 @@ class Chessboard(QGraphicsView):
                 flip_adjusted_j = j if self.flipped else 7 - j
                 flip_adjusted_i = 7-i if self.flipped else i
                 piece = self.game_state.board.piece_at(chess.square(flip_adjusted_i, flip_adjusted_j))
-                # print(f"I'm at {i} {j} and got piece {piece}")
                 if piece is not None:
-                    # print(f'At position {i} {j} there is {piece}')
-                    # print(f'Trying to get piece {piece} at square {i}x{j}')
                     image = QImage(self.piece_translation[str(piece)])
                     scaled_image = image.scaled(
                         int(self.squares[i][j].rect().width()),
@@ -158,19 +147,12 @@ class Chessboard(QGraphicsView):
                         QtCore.Qt.TransformationMode.SmoothTransformation  # High quality filter
                     )
                     pixmap_item = QGraphicsPixmapItem(QPixmap.fromImage(scaled_image))
-                    # pixmap = QPixmap(self.piece_translation[str(piece)])
-                    # pixmap_item = QGraphicsPixmapItem(pixmap.scaled(
-                    #     int(self.squares[i][j].rect().width()),
-                    #     int(self.squares[i][j].rect().height()),
-                    #     Qt.AspectRatioMode.KeepAspectRatio
-                    # ))
                     pixmap_item.square = self.squares[i][j]
                     pixmap_item.setPos(self.squares[i][j].rect().left(), self.squares[i][j].rect().top())
                     self.scene.addItem(pixmap_item)
                     self.pieces[i][j] = pixmap_item
 
     def draw_board(self):
-        # print('Entered draw board')
         for row in range(8):
             for col in range(8):
                 square = self.squares[row][col]
@@ -186,7 +168,6 @@ class Chessboard(QGraphicsView):
 
         for i in range(8):
             for j in range(8):
-                # rect = QGraphicsRectItem(QRectF(i * rect_width, j * rect_height, rect_width, rect_height))
                 rect = ChessSquare(i, j, QRectF(i * rect_width, j * rect_height, rect_width, rect_height))
                 pen = QPen()
                 pen.setStyle(Qt.PenStyle.NoPen)
@@ -194,8 +175,6 @@ class Chessboard(QGraphicsView):
                 rect.setBrush(colors[((i % 2) + j) % 2])
                 self.scene.addItem(rect)
                 self.squares[i][j] = rect
-        # print("After draw_board()")
-        # print(self.squares)
         self.refresh_board()
 
     def redraw_board(self):
